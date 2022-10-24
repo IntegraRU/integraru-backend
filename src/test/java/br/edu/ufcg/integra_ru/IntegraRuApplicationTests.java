2c9ed13a496c68dcad16e3e1009d5594f5e796e7
Eduardo <eduardo.afonso.silva@ccc.ufcg.edu.br>
2022-10-24 20:55:26 -0300

    base backend

---
+++ src/test/java/br/edu/ufcg/integra_ru/IntegraRuApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ufcg.integra_ru;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntegraRuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
