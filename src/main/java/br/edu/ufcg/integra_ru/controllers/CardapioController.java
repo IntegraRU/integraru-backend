08fda4caff8452545638cd23fc7238d5937236bb
Eduardo <eduardo.nunes.silva@virtus.ufcg.edu.br>
2022-11-09 13:52:10 -0300

    continuação cardapio

--- src/main/java/br/edu/ufcg/integra_ru/controllers/CardapioController.java
+++ src/main/java/br/edu/ufcg/integra_ru/controllers/CardapioController.java
@@ -13,7 +13,7 @@ import java.net.URI;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/cardapios")
+@RequestMapping("/api/cardapio")
 public class CardapioController {
 
     @Autowired
