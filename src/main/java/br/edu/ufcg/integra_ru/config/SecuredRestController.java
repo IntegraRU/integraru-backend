6311a065910d34614ab3e7ac13a3e8d94ca987b2
Eduardo Nunes <eduardo.afonso.silva@ccc.ufcg.edu.br>
2022-12-04 16:46:38 -0300

    documentação

--- src/main/java/br/edu/ufcg/integra_ru/config/SecuredRestController.java
+++
@@ -1,7 +0,0 @@
-package br.edu.ufcg.integra_ru.config;
-
-import io.swagger.v3.oas.annotations.security.SecurityRequirement;
-
-@SecurityRequirement(name = "bearerAuth")
-public interface SecuredRestController {
-}
